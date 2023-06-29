0197d74b4326f56b1f213889eacc40022981e99d
linder3hs <linderhassinger00@gmail.com>
2023-06-29 15:06:38 -0500

    initial commit

---
+++ src/app/api/spotify/route.ts
@@ -0,0 +1,18 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
+
+export async function GET(_request: NextRequest) {
+  const response = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: `grant_type=client_credentials&client_id=${client_id}&client_secret=${client_secret}`,
+  });
+
+  const json_response = await response.json();
+
+  return NextResponse.json(json_response);
+}
