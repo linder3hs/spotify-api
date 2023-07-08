970405890e5763ce927fde4420b8ce3729790c00
linder3hs <linderhassinger00@gmail.com>
2023-07-08 14:12:11 -0500

    add playlist

--- src/app/api/playlist/route.ts
+++ src/app/api/playlist/route.ts
@@ -7,28 +7,8 @@ export async function GET(_request: NextRequest) {
 
 export async function POST(request: NextRequest) {
   const res = await request.json();
-  const newPlaylist = {
-    images: [
-      {
-        height: 640,
-        url: "https://mosaic.scdn.co/640/ab67616d0000b2735c043f98ad687480ca6f0441ab67616d0000b27386afd6f8f6007e35cc882d2dab67616d0000b27390a704b3e697867936246209ab67616d0000b273cbca07f8e1eefdbbe554279e",
-        width: 640,
-      },
-      {
-        height: 300,
-        url: "https://mosaic.scdn.co/300/ab67616d0000b2735c043f98ad687480ca6f0441ab67616d0000b27386afd6f8f6007e35cc882d2dab67616d0000b27390a704b3e697867936246209ab67616d0000b273cbca07f8e1eefdbbe554279e",
-        width: 300,
-      },
-      {
-        height: 60,
-        url: "https://mosaic.scdn.co/60/ab67616d0000b2735c043f98ad687480ca6f0441ab67616d0000b27386afd6f8f6007e35cc882d2dab67616d0000b27390a704b3e697867936246209ab67616d0000b273cbca07f8e1eefdbbe554279e",
-        width: 60,
-      },
-    ],
-    ...res,
-  };
-
-  playlist.items.push(newPlaylist);
+  
+  playlist.items.push(res);
 
   return NextResponse.json(playlist);
 }
